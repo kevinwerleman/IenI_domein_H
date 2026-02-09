a714b954ecc155c11205d68b19c3d00473f95092
kevin <113090381+kevinwerleman@users.noreply.github.com>
2026-02-09 13:37:31 +0000

    4

--- public/test.php
+++ public/test.php
@@ -1,4 +1,23 @@
 <?php
-// echo "Hallo Kevin";
-PHPinfo();
+// echo "Hallo";
+//PHPinfo();
+
+$mysqli = new mysqli("127.0.0.1", "user", "password","mobiel");
+
+$result = $mysqli->query("SELECT id FROM aankopen");
+
 ?>
+
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <meta charset="utf8">
+</head>
+         <?php  
+            while ($row = $result->fetch_assoc()) {
+               echo $row['id'] .  "<br>";
+             }
+         ?>
+</body>
+</html>
