db6d0fd81840f84feff9ec0fb088508ecb5b87f0
kevin <113090381+kevinwerleman@users.noreply.github.com>
2026-03-01 12:57:02 +0000

    6

--- workflows/index_workflows.php
+++ workflows/index_workflows.php
@@ -1,36 +1,23 @@
 <?php
 
-//---voor errir
+//---voor errir testen
 // ini_set('display_errors', 1);
 // ini_set('display_startup_errors', 1);
 // error_reporting(E_ALL);
-//-------------
+// -------------
 
 require_once __DIR__ . '/../workflows/AI_NLM/vendor/autoload.php';
 
 class MyVader extends \TextAnalysis\Sentiment\Vader {
     protected function getTxtFilePath() : string
     {
-        return __DIR__ . '/../storage/sentiment/vader_lexicon/vader_lexicon.txt';
+        return __DIR__ . '/../vaderSentiment-3.3.2/vaderSentiment/vader_lexicon.txt';
     }
 }
 
-//aleen dit aanraken voor database invoegen!!!!!
-$mysqli = new mysqli("127.0.0.1", "user", "password", "voornamen");
-$result = $mysqli->query("SELECT naam FROM namen");
+$mysqli = new mysqli("127.0.0.1", "user", "password", "KLANTENBERICHTEN EIND");
+$result = $mysqli->query("SELECT inhoud FROM klantenberichten");
 
-$tekst = "the product was cool.";
-$tokens = tokenize($tekst);
-//!!!!!!!!!!!!!!!!!!
-
-$tokens = array_map(function($token) {
-    return preg_replace('/[^\p{L}\p{N}]/u', '', $token);
-}, $tokens);
-$tokens = array_filter($tokens);
-$tokens = array_values($tokens);
-
-$vader = new MyVader();
-$sentiment = $vader->getPolarityScores($tokens);
 ?>
 
 
@@ -40,22 +27,45 @@ $sentiment = $vader->getPolarityScores($tokens);
     <meta charset="utf8">
 </head>
 <body>
+
     <h3>positief of negatief:</h3>
-    <p>Tekst: <?= htmlspecialchars($tekst) ?></p>
-    <ul>
-        <li>Positief: <?= $sentiment['pos'] ?></li>
-        <li>Negatief: <?= $sentiment['neg'] ?></li>
-        <li>Neutraal: <?= $sentiment['neu'] ?></li>
-        <li>Compound: <?= $sentiment['compound'] ?></li>
-    </ul>
-    <p><strong>Conclusie: <?= ($sentiment['pos'] > $sentiment['neg']) ? 'Positief' : 'Negatief' ?></strong></p>
+    
+<?php
+while ($row = $result->fetch_assoc()): 
 
-    <?php  
-            while ($row = $result->fetch_assoc()) {
-               echo $row['naam'] .  "<br>";
-             }
-    ?>
+    echo $row['inhoud'] .  "<br>";
+    //$tekst = $row['inhoud'];
+
+    $tekst = "I love this product!";
+    $tokens = tokenize($tekst);
+    //!!!!!!!!!!!!!!!!!!
 
 
+    $tokens = array_map(function($token) {
+    return preg_replace('/[^\p{L}\p{N}]/u', '', $token);
+    }, $tokens);
+    $tokens = array_filter($tokens);
+    $tokens = array_values($tokens);
+
+    $vader = new MyVader();
+    $sentiment = $vader->getPolarityScores($tokens);
+
+?>
+
+        <div class="bericht">
+            <p><strong>Tekst:</strong> <?= htmlspecialchars($tekst) ?></p>
+            <ul class="scores">
+                <li>Positief: <?= $sentiment['pos'] ?></li>
+                <li>Negatief: <?= $sentiment['neg'] ?></li>
+                <li>Neutraal: <?= $sentiment['neu'] ?></li>
+                <li>Compound: <?= $sentiment['compound'] ?></li>
+            </ul>
+            <p><strong>Conclusie:</strong> 
+                <?= ($sentiment['pos'] > $sentiment['neg']) ? 'Positief' : 'Negatief' ?>
+            </p>
+        </div>
+        
+    <?php endwhile; ?>
 </body>
 </html>
+
